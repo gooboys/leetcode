b1770647cc628ab0ced2d6f9459efb4af4aa00e9
Thierry Juang <thierry.tj007@gmail.com>
2025-02-07 08:05:59 -0500

    adding the two problems I did today

---
+++ Binary_Search/Search_Insert_Position.py
@@ -0,0 +1,46 @@
+'''
+35. Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+nums contains distinct values sorted in ascending order.
+-10^4 <= target <= 10^4
+'''
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = right+left//2
+            if nums[mid]>target:
+                right = mid-1
+            elif nums[mid]<target:
+                left = mid+1
+            else:
+                return mid
+        return left
